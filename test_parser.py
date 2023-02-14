8480d29a2a096b78d5d305a27a64f6a47528bce4
Stepa <stepa8000@gmail.com>
2023-02-14 17:34:30 +0400

    add tests

--- test_parser.py
+++ test_parser.py
@@ -1,4 +1,5 @@
-from parser import Parser
+from decoder import JSONDecoder
+
 
 def test_parse_value():
     values = {
@@ -11,16 +12,42 @@ def test_parse_value():
         "[]": []
     }
     for k, v in values.items():
-        P = Parser(k)
-        assert P.parse_value() == v
+        d = JSONDecoder(k)
+        assert d.parse_value() == v
+
 
 def test_parse_number():
-    ints = ["123456", "0", "1", "2", "5", "-54321", "-9", "-8", "-0", "1000002", "500000"]
+    ints = ["123456", "0", "1", "2", "5", "-54321",
+            "-9", "-8", "-0", "1000002", "500000"]
     for n in ints:
-        P = Parser(n)
-        assert P.parse_number() == int(n)
+        d = JSONDecoder(n)
+        assert d.parse_number() == int(n)
 
-    floats = ["123.0", "-2345.0", "0.0005e+3", "98769e-2", "-0.0", "1.0405006E+005"]
+    floats = ["123.0", "-2345.0", "0.0005e+3",
+              "98769e-2", "-0.0", "1.0405006E+005"]
     for n in floats:
-        P = Parser(n)
-        assert P.parse_number() == float(n.lower())  # 'e', not 'E'
+        d = JSONDecoder(n)
+        assert round(d.parse_number(), 5) == float(n.lower())  # 'e', not 'E'
+
+
+def test_parse_member():
+    pairs = {
+        '"a":true': ("a", True),
+        '"b":0': ("b", 0),
+        '"":null': ("", None),
+        '"key":{}': ("key", {})
+    }
+    for k, v in pairs.items():
+        d = JSONDecoder(k)
+        assert d.parse_member() == v
+
+
+def test_parse_array():
+    arrays = {
+        "[]": [],
+        "[true,\nfalse,\nnull,\n{}]": [True, False, None, {}],
+        "[\t1,\t2,\t\"string\", 34, 5.3]": [1, 2, "string", 34, 5.3]
+    }
+    for k, v in arrays.items():
+        d = JSONDecoder(k)
+        assert d.parse_array() == v
